ac55302380b2ae225962f57ad915bd1d457d1b72
Samuel Gaamuwa <sgaamuwa@gmail.com>
2015-08-18 15:48:48 +0300

    pointed the profile tab to the student profile page

--- DefaultPage.php
+++ DefaultPage.php
@@ -13,28 +13,32 @@
         <title></title>
     </head>
     <body>
-        <header>
-            <h1>STUDENT MONITORING APPLICATION</h1>
-        </header>
-        <nav class="navbar navbar-default" role="navigation">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="">Student's monitoring app</a>
-            </div>
-            <div>
-                <ul class="nav navbar-nav">
-                    <li id="home"><a href="">HOME</a></li>
-                    <li id="profile"><a href="">PROFILE</a></li>
-                    <li id="classes"><a href="">CLASSSES</a></li>
-                    <li id="messages"><a href="">MESSAGES</a></li>
-                    <li id="student" class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="">STUDENT<span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li id="sprofile"><a href="">Profile</a></li>
-                            <li id="reg"><a href="/RegistrationPage.php">Registration</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <div class="container">
+            <header>
+                <h1>STUDENT MONITORING APPLICATION</h1>
+            </header>
+        </div>
+        <div class="container">
+             <nav class="navbar navbar-default" role="navigation">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="">Student's monitoring app</a>
+                </div>
+                <div>
+                    <ul class="nav navbar-nav">
+                        <li id="home"><a href="">HOME</a></li>
+                        <li id="profile"><a href="">PROFILE</a></li>
+                        <li id="classes"><a href="">CLASSSES</a></li>
+                        <li id="messages"><a href="">MESSAGES</a></li>
+                        <li id="student" class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="">STUDENT<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li id="sprofile"><a href="/StudentProfile.php">Profile</a></li>
+                                <li id="reg"><a href="/RegistrationPage.php">Registration</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     </body>
 </html>
