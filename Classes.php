529be7c49bf9b468fdcd21de8815795e500790cc
Samuel Gaamuwa <samuelgaamuwa@hotmail.com>
2015-08-20 15:12:37 +0300

    new classes page added to display the different students and probably a few details

---
+++ Classes.php
@@ -0,0 +1,259 @@
+<?php
+include 'DefaultPage.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div id ="wrapper" class="container">
+                    <div id="sidebar-wrapper" class="col-lg-3 bs-docs-sidebar">
+                        <ul class="nav nav-list bs-docs-sidenav">
+                            <li><a href="#s6">S6 Class</a></li>
+                            <li><a href="#s5">S5 Class</a></li>
+                            <li><a href="#s4">S4 Class</a></li>
+                            <li><a href="#s3">S3 Class</a></li>
+                            <li><a href="#s2">S2 Class</a></li>
+                            <li><a href="#s1">S1 Class</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-9">
+                        <section id="s6">
+                            <div class="page-header">
+                                <h1>S6 Class</h1>
+                            </div>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Student Number</th>
+                                        <th>Sex</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Mathematics</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Physics</td>
+                                        </tr>
+                                    <tr>
+                                        <td>English</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Biology</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Chemistry</td>
+                                        </tr>
+                                    <tr>
+                                        <td>History</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Geography</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Languages</td>
+                                        </tr>
+                                </thead>
+                            </table>
+                        </section>
+                        <section id="s5">
+                            <div class="page-header">
+                                <h1>S5 Class</h1>
+                            </div>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Student Number</th>
+                                        <th>Sex</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Mathematics</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Physics</td>
+                                        </tr>
+                                    <tr>
+                                        <td>English</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Biology</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Chemistry</td>
+                                        </tr>
+                                    <tr>
+                                        <td>History</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Geography</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Languages</td>
+                                        </tr>
+                                </thead>
+                            </table>
+                        </section>
+                        <section id="s4">
+                            <div class="page-header">
+                                <h1>S4 Class</h1>
+                            </div>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Student Number</th>
+                                        <th>Sex</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Mathematics</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Physics</td>
+                                        </tr>
+                                    <tr>
+                                        <td>English</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Biology</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Chemistry</td>
+                                        </tr>
+                                    <tr>
+                                        <td>History</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Geography</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Languages</td>
+                                        </tr>
+                                </thead>
+                            </table>
+                        </section>
+                        <section id="s3">
+                            <div class="page-header">
+                                <h1>S3 Class</h1>
+                            </div>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Student Number</th>
+                                        <th>Sex</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Mathematics</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Physics</td>
+                                        </tr>
+                                    <tr>
+                                        <td>English</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Biology</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Chemistry</td>
+                                        </tr>
+                                    <tr>
+                                        <td>History</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Geography</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Languages</td>
+                                        </tr>
+                                </thead>
+                            </table>
+                        </section>
+                        <section id="s2">
+                            <div class="page-header">
+                                <h1>S2 Class</h1>
+                            </div>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Student Number</th>
+                                        <th>Sex</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Mathematics</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Physics</td>
+                                        </tr>
+                                    <tr>
+                                        <td>English</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Biology</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Chemistry</td>
+                                        </tr>
+                                    <tr>
+                                        <td>History</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Geography</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Languages</td>
+                                        </tr>
+                                </thead>
+                            </table>
+                        </section>
+                        <section id="s1">
+                            <div class="page-header">
+                                <h1>S1 Class</h1>
+                            </div>
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Student Number</th>
+                                        <th>Sex</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Mathematics</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Physics</td>
+                                        </tr>
+                                    <tr>
+                                        <td>English</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Biology</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Chemistry</td>
+                                        </tr>
+                                    <tr>
+                                        <td>History</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Geography</td>
+                                        </tr>
+                                    <tr>
+                                        <td>Languages</td>
+                                        </tr>
+                                </thead>
+                            </table>
+                        </section>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
